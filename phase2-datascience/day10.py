b66943b42b2d6db8f1b26ce9a385da5c0711aaea
Rahul Mhatre <rahulcodes01@gmail.com>
2026-05-27 19:56:39 +0530

    Learnt data cleaning by dropping negative ages(wrong data entry), filling in empty columns by median and mode and flagging outliers(something that seems clinically rare is then flagged for review)

---
+++ phase2-datascience/day10.py
@@ -0,0 +1,31 @@
+import pandas as pd
+#Data cleaning
+df = pd.read_csv('heart.csv')
+
+# 'fbs' is Fasting Blood Sugar
+
+median_bs= df['fbs'].median()
+df['fbs']=df['fbs'].fillna(median_bs)
+
+
+df=df[df['age']>=0]
+median_chol= df['chol'].median()
+df['chol']=df['chol'].fillna(median_chol)
+
+def flag_clinical_outliers(df, column):
+    new_column_name = f"{column}_outlier"
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    upper_bound = Q3 + 1.5 * IQR
+    lower_bound = Q1 - 1.5 * IQR
+    df[new_column_name]=( ((df[column] > upper_bound) | (df[column] < lower_bound)).astype(int))
+    return df
+
+
+for i in df.keys():
+    if i=='target'or i=='sex'or i=='cp':
+        continue
+    flag_clinical_outliers(df,i)
+
+print(df['ca'])
